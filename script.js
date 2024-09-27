ccf0f6b084d5c053afe0aec5af889049691e9e89
Neeraj Shrivas <shrivasneeraj01@gmail.com>
2024-09-27 23:08:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+
+let btns = document.querySelectorAll(".btn");
+let currentAudio = null;
+btns.forEach(t => {
+    // console.log(t.innerHTML);
+    let audio = document.createElement("audio");
+    audio.src = `sounds/${t.innerHTML}.mp3`;
+    // console.log(audio);
+    
+    t.addEventListener("click", (e) => {
+        e.preventDefault();
+        if (currentAudio) {
+            currentAudio.pause();
+            currentAudio.currentTime = 0;
+        }
+        currentAudio = audio
+        currentAudio.play();
+    })
+    
+});
+
+let stop = document.querySelector(".stop");
+
+stop.addEventListener("click", () => {
+    if (currentAudio) {
+        currentAudio.pause();
+        currentAudio.currentTime = 0;
+    }
+})
